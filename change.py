2b85fa3fd187325fee3bc29d5b257b8c859a3cfe
Eliseo-Laborde <Elaborde1@mail.austral.edu.ar>
2025-03-28 10:56:52 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,13 @@
 def change():
     expense = 23.75
     money = 100
+    change = money - expense
+    print("Ingresar gasto:")
+    print(expense)
+    print("Dinero recibido")
+    print(money)
+    print("\nVuelto\n")
+    print("Pesos:")
+    print(change)
+    print("Centavos:")
+    print((change - 76)*100)
